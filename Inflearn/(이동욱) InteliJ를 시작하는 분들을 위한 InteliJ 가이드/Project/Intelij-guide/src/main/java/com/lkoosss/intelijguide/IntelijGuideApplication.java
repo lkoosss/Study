201d3a90a3207e38d44837998540ea33e89b9c16
lkoosss <lkoosss@gmail.com>
2022-06-03 00:47:38 +0900

    2

---
+++ Inflearn/(이동욱) InteliJ를 시작하는 분들을 위한 InteliJ 가이드/Project/Intelij-guide/src/main/java/com/lkoosss/intelijguide/IntelijGuideApplication.java
@@ -0,0 +1,13 @@
+package com.lkoosss.intelijguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelijGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelijGuideApplication.class, args);
+	}
+
+}
---
+++ Inflearn/(이동욱) InteliJ를 시작하는 분들을 위한 InteliJ 가이드/Project/Intelij-guide/src/test/java/com/lkoosss/intelijguide/IntelijGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkoosss.intelijguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelijGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
