58ffc210a3ca1b4da56884c50c379be36db2961a
lkoosss <lkoosss@gmail.com>
2022-09-08 21:28:48 +0900

    Header완성, Visual 첫번째 완성

--- FastCamp/(박영웅,이웅재) 한번에 끝내는 프론트엔드 개발/starbucks/js/main.js
+++ FastCamp/(박영웅,이웅재) 한번에 끝내는 프론트엔드 개발/starbucks/js/main.js
@@ -14,3 +14,22 @@ searchInputEl.addEventListener('blur', () => {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder','');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(() => {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        // 배지 숨기기
+        gsap.to(badgeEl, 0.6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        // 배지 보이기
+        gsap.to(badgeEl, 0.6, {
+            opacity: 1,
+            display: 'block'
+        })
+    }
+}, 300));
