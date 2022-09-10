12e6432a5705063d32f33150cdb86b1d7fbaff27
lkoosss <lkoosss@gmail.com>
2022-09-10 20:49:35 +0900

    almost complete

--- FastCamp/(박영웅,이웅재) 한번에 끝내는 프론트엔드 개발/starbucks/js/main.js
+++ FastCamp/(박영웅,이웅재) 한번에 끝내는 프론트엔드 개발/starbucks/js/main.js
@@ -18,7 +18,6 @@ searchInputEl.addEventListener('blur', () => {
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(() => {
-    console.log(window.scrollY);
     if (window.scrollY > 500) {
         // 배지 숨기기
         gsap.to(badgeEl, 0.6, {
@@ -33,3 +32,77 @@ window.addEventListener('scroll', _.throttle(() => {
         })
     }
 }, 300));
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach((item, index) => {
+    gsap.to(item, 1, {
+        delay: (index + 1) * 0.7,
+        opacity: 1
+    });
+});
+
+
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true,
+});
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 부여
+        clickable: true,    // 사용자 페이지 번호 요소 제어
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+})
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', () => {
+    isHidePromotion = !isHidePromotion;
+
+    if (isHidePromotion) {
+        promotionEl.classList.add('hide');
+    } else {
+        promotionEl.classList.remove('hide');
+    }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+  }
+
+function floatingObject(selector, delay, size) {
+    gsap.to(
+        selector, 
+        random(1.5, 2.5), 
+        {
+            y: size,
+            repeat: -1,
+            yoyo: true,
+            ease: Power1.easeInOut,
+            delay: random(0, delay)
+        }
+    );
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
